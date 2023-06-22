240de31bb32715f7f0d796cc5152b68e2b413059
hakan-persson <hakan@mabadiliko.se>
2023-06-22 11:03:33 +0200

    Added signal handling

--- pyapp/main.py
+++ pyapp/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import signal
 import asyncio
 
 from app import websocket_server
@@ -17,25 +18,20 @@ HTTP_STATIC_DIR = os.getenv('HTTP_STATIC_DIR', "./jsapp/out")
 """
 MAIN function (starting point)
 """
-def main():                 # noqa:E302
-    # Enable logging. INFO is default. DEBUG if requested
-    logging.basicConfig(level=logging.DEBUG if DEBUG_MODE else logging.INFO, format=LOGFORMAT)
-
-    loop = asyncio.new_event_loop()         # Create an async loop
-    # loop = asyncio.get_event_loop()         # Create an async loop
+async def main():                 # noqa:E302
+    loop = asyncio.get_running_loop()
+    
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
     websocket_runner = loop.create_task(websocket_server(WEBSOCKET_SERVER_PORT))
     http_runner = loop.create_task(http_server(HTTP_SERVER_PORT, HTTP_STATIC_DIR))        # Create http server # noqa:F841
 
-    try:
-        asyncio.set_event_loop(loop)        # Make the create loop the event loop
-        loop.run_forever()                  # And run everything
-    except (KeyboardInterrupt):             # Until somebody hits wants to terminate
-        pass
-    finally:
-        loop.run_until_complete(loop.shutdown_asyncgens())
-        loop.close()
-
+    await stop      # Run forever until stopped!
 
 if __name__ == "__main__":
-    main()
+    # Enable logging. INFO is default. DEBUG if requested
+    logging.basicConfig(level=logging.DEBUG if DEBUG_MODE else logging.INFO, format=LOGFORMAT)
+
+    asyncio.run(main())     # Start an async loop and run main
